d58b3e0f6c3fc9da68844c21e2efe51b7760fd13
oscarhere <Oscar@Oscars.local>
2015-05-01 12:17:41 +0200

    la till 4e nslog"test4"

--- ProjektDemo/ViewController.swift
+++ ProjektDemo/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         
         NSLog(@"test2");
         NSLog(@"test3");
+        NSLog(@"test4");
         // Do any additional setup after loading the view, typically from a nib.
     }
 
