895c01dd689940b00d3368a065eb0a66d4ee10a1
oscarhere <Oscar@Oscars.local>
2015-05-01 12:13:27 +0200

    andra
    Lade till test2 Nslog

--- ProjektDemo/ViewController.swift
+++ ProjektDemo/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         println("tjena")
+        
+        NSLog(@"test2");
         // Do any additional setup after loading the view, typically from a nib.
     }
 
