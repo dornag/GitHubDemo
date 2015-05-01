bec791d8aac5ea106019d639adc5278a5d430b64
oscarhere <Oscar@Oscars.local>
2015-05-01 12:08:40 +0200

    första
    testar med println(“tjena”)

---
+++ ProjektDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ProjektDemo
+//
+//  Created by Oscar Nilsson on 2015-05-01.
+//  Copyright (c) 2015 Oscar Nilsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("tjena")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
